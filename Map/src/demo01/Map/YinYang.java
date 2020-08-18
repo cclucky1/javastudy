943c2ac9879641679146f0510613ea3c199bbff5
cccchen <fade@tom.com>
2020-08-18 23:56:49 +0800

    Add GitHub and new class

---
+++ Map/src/demo01/Map/YinYang.java
@@ -0,0 +1,13 @@
+package demo01.Map;
+
+public class YinYang {
+    public static void main(String[] args) {
+        jiuzhe();
+    }
+
+    private static void jiuzhe() {
+        for (int i = 250; i > 213; i++) {
+            System.out.println("就这?");
+        }
+    }
+}
